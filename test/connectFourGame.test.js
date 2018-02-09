235758f46dab4b7240e90d3b7ca2ecadf7f66efd
sameer girkar <sgirkar@apple.com>
2018-02-09 09:50:46 -0800

    add unit test cases

---
+++ test/connectFourGame.test.js
@@ -0,0 +1,12 @@
+describe("ConnectFourGame", function () {
+  var grid;
+  describe("constructor function test suit,", function() {
+    beforeEach(function () {
+      grid = new GRID();
+    });
+    it("should be able to Create empty grid", function () {
+
+      expect(true).toBe(true);
+    });
+  });
+});
